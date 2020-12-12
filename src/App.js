abf3525cb8719b4437bc117c596c4b63146c6ba1
tasawernawaz <tasawer.nawaz@arbisoft.com>
2020-12-12 10:22:08 +0500

    initial state management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContact from './ListContacts'
+
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+
+
+  removeContact = (contact) => {
+    this.setState(currentState => ({
+        contacts :currentState.contacts.filter(c => contact.id !== c.id)
+      })
+    );
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContact
+          contacts={this.state.contacts}
+          onClickRemoveContact={this.removeContact}
+        />
       </div>
     );
   }
